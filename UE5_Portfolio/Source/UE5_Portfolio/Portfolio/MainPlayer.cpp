293cd1d6bbdcd3af3da9d2770ae70b7372fa4436
Jeon <dbs_dmswl92@naver.com>
2023-07-11 03:05:51 +0900

    Set Temp SkeletalMesh

--- UE5_Portfolio/Source/UE5_Portfolio/Portfolio/MainPlayer.cpp
+++ UE5_Portfolio/Source/UE5_Portfolio/Portfolio/MainPlayer.cpp
@@ -14,28 +14,62 @@
 AMainPlayer::AMainPlayer()
 {
 	// Do not Rotate Character to Camera direction
-	bUseControllerRotationPitch = false;
-	bUseControllerRotationYaw = false;
-	bUseControllerRotationRoll = false;
-
+	{
+		bUseControllerRotationPitch = false;
+		bUseControllerRotationYaw = false;
+		bUseControllerRotationRoll = false;
+	}
+	
 	//Configure Character Movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Rotate character to moving direction
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 640.0f, 0.0f);
-	GetCharacterMovement()->bConstrainToPlane = true;
-	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+	{
+		GetCharacterMovement()->bOrientRotationToMovement = true; // Rotate character to moving direction
+		GetCharacterMovement()->RotationRate = FRotator(0.0f, 640.0f, 0.0f);
+		GetCharacterMovement()->bConstrainToPlane = true;
+		GetCharacterMovement()->bSnapToPlaneAtStart = true;
+	}
+	
 
 	// Create SpringArm
-	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SPRINGARM"));
-	SpringArmComponent->SetupAttachment(RootComponent);
-	SpringArmComponent->SetUsingAbsoluteRotation(true); //Don't want arm to rotate when character does
-	SpringArmComponent->TargetArmLength = 1250.0f;
-	SpringArmComponent->SetRelativeRotation(FRotator(-60.0f, 0.0f, 0.0f));
-	SpringArmComponent->bDoCollisionTest = false; // Don't want ot pull camera in when it collides with level
+	{
+		SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SPRINGARM"));
+		SpringArmComponent->SetupAttachment(RootComponent);
+		SpringArmComponent->SetUsingAbsoluteRotation(true); //Don't want arm to rotate when character does
+		SpringArmComponent->TargetArmLength = 1250.0f;
+		SpringArmComponent->SetRelativeRotation(FRotator(-60.0f, 0.0f, 0.0f));
+		SpringArmComponent->bDoCollisionTest = false; // Don't want ot pull camera in when it collides with level
+	}
+	
+	
+	
 
 	//Create Camera
-	TopDownCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("TOPDOWNCAMERA"));
-	TopDownCameraComponent->SetupAttachment(SpringArmComponent);
-	TopDownCameraComponent->bUsePawnControlRotation = false; // Camera does not rotaterelative to arm
+	{
+		TopDownCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("TOPDOWNCAMERA"));
+		TopDownCameraComponent->SetupAttachment(SpringArmComponent);
+		TopDownCameraComponent->bUsePawnControlRotation = false; // Camera does not rotaterelative to arm
+	}
+	
+	
+
+	//SetSkeletalMesh
+	{
+		static ConstructorHelpers::FObjectFinder<USkeletalMesh> SkelMesh(
+			TEXT("/Script/Engine.SkeletalMesh'/Game/ParagonCountess/Characters/Heroes/Countess/Meshes/SM_Countess.SM_Countess'")
+		);
+
+		if (SkelMesh.Succeeded())
+		{
+			GetMesh()->SetSkeletalMesh(SkelMesh.Object);
+		}
+		else
+		{
+			UE_LOG(LogTemp, Log, TEXT("%s(%u) MainPlayer : Can not found Skeletal Mesh"), __FUNCTION__, __LINE__);
+		}
+	}
+	
+
+	
+
 
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
