929eb470a788df96bf6b6ae14ccd76bd0a1037c0
JHW096 <JHW096@gmail.com>
2023-07-24 12:11:04 +0900

    Create BTTask_NormalMonster_Return file, Remove Trash file

--- UE5_Portfolio/Source/UE5_Portfolio/Monster/AI/BTTask_NormalMonster_Return.cpp
+++ UE5_Portfolio/Source/UE5_Portfolio/Monster/AI/BTTask_NormalMonster_Return.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "BTTask_BaseNode_Attack.h"
+#include "BTTask_NormalMonster_Return.h"
 
--- UE5_Portfolio/Source/UE5_Portfolio/Monster/AI/BTTask_NormalMonster_Return.h
+++ UE5_Portfolio/Source/UE5_Portfolio/Monster/AI/BTTask_NormalMonster_Return.h
@@ -4,13 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "BTTask_BaseNode.h"
-#include "BTTask_BaseNode_Attack.generated.h"
+#include "BTTask_NormalMonster_Return.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class UE5_PORTFOLIO_API UBTTask_BaseNode_Attack : public UBTTask_BaseNode
+class UE5_PORTFOLIO_API UBTTask_NormalMonster_Return : public UBTTask_BaseNode
 {
 	GENERATED_BODY()
 	
